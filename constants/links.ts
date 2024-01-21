fa3ee9bf75270e198dc3cfb82a38d1c949c0aea8
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-01-21 23:33:36 +0500

    Add sidebar links to constants

---
+++ constants/links.ts
@@ -0,0 +1,32 @@
+export const Sidebar_Links = [
+  {
+    title: "Home",
+    href: "/",
+    image: "/assets/icons/home.svg",
+  },
+  {
+    title: "Community",
+    href: "/community",
+    image: "/assets/icons/users.svg",
+  },
+  {
+    title: "Collections",
+    href: "/collections",
+    image: "/assets/icons/star.svg",
+  },
+  {
+    title: "Find Jobs",
+    href: "/jobs",
+    image: "/assets/icons/suitcase.svg",
+  },
+  {
+    title: "Tags",
+    href: "/tags",
+    image: "/assets/icons/tag.svg",
+  },
+  {
+    title: "Ask a Question",
+    href: "/ask",
+    image: "/assets/icons/question.svg",
+  },
+];
