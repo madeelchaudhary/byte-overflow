f76e51798a4faf23b48687220e96404e3eb19343
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-01-21 23:34:26 +0500

    Add navbar and sidebars to layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,20 @@
+import Navbar from "@/components/shared/navbar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <div className="background-light850_dark100 relative">
+      <Navbar />
+      <main className="flex">
+        <aside>Left Sidebar</aside>
+        <section className="flex min-h-screen grow flex-col px-6 pb-14 pt-36 sm:px-14 md:pb-6">
+          {children}
+        </section>
+        <aside>Right Sidebar</aside>
+      </main>
+      Toaster
+    </div>
+  );
 };
 
 export default layout;
