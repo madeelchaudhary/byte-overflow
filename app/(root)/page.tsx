fb94d321c201494cd441d8b52eb847b0650636c3
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-01-27 14:04:41 +0500

    Add search functionality and "Ask a Question" button to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="h1-bold">Next.js 14</h1>
-    </main>
+    <>
+      <div className=" flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex flex-col justify-between gap-5 sm:flex-row sm:items-center ">
+        <LocalSearch
+          className="flex-1"
+          placeholder="Search for questions"
+          route="/"
+          img="/assets/icons/search.svg"
+          imgSide="left"
+        />
+      </div>
+    </>
   );
 }
