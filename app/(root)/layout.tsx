8972722d5350e9493eca856896d8775332cace18
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-01-28 20:37:05 +0500

    Add left and right sidebars to layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/shared/navbar";
+import LeftSideBar from "@/components/shared/sidebars/LeftSidebar";
+import RightSideBar from "@/components/shared/sidebars/RightSidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
@@ -6,13 +8,13 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     <div className="background-light850_dark100 relative">
       <Navbar />
       <main className="flex">
-        <aside>Left Sidebar</aside>
+        <LeftSideBar />
         <section className="flex min-h-screen grow flex-col px-6 pb-14 pt-36 sm:px-14 md:pb-6">
           {children}
         </section>
-        <aside>Right Sidebar</aside>
+        <RightSideBar />
       </main>
-      Toaster
+      {/* Toaster */}
     </div>
   );
 };
