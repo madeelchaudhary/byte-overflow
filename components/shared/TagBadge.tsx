7b5d33c47a7bde8018d95012429803a0cb1e42b3
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-01-29 16:57:18 +0500

    Add TagBadge component

---
+++ components/shared/TagBadge.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import { Badge } from "../ui/badge";
+
+interface Props {
+  _id: number;
+  name: string;
+  totalQuestions?: number;
+  renderCount?: boolean;
+}
+
+const TagBadge = ({ _id, name, totalQuestions, renderCount }: Props) => {
+  return (
+    <Link
+      className="flex items-center justify-between gap-2"
+      href={`/tags/${_id}`}
+    >
+      <Badge className="subtle-medium text-light400_light500 background-light800_dark300 rounded-md border-none px-4 py-2">
+        {name}
+      </Badge>
+      {renderCount && (
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+      )}
+    </Link>
+  );
+};
+
+export default TagBadge;
