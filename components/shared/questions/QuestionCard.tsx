961f149d58878a32227a1b1e4b10d722fc59c696
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-01 13:12:29 +0500

    Add QuestionCard component

---
+++ components/shared/questions/QuestionCard.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+import React from "react";
+import TagBadge from "../TagBadge";
+import Metric from "../Metric";
+import { humanizeDate, humanizeNumber } from "@/lib/utils";
+
+interface Props {
+  _id: string;
+  title: string;
+  tags: Array<{ _id: string; name: string }>;
+  answers: Array<object>;
+  views: number;
+  upvotes: number;
+  author: { _id: string; name: string; picture: string };
+  created_at: Date;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  answers,
+  author,
+  upvotes,
+  views,
+  created_at,
+}: Props) => {
+  return (
+    <div className="card-wrapper rounded-xl p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 inline-block sm:hidden">
+            {humanizeDate(created_at)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+        {/* allow edit if author */}
+      </div>
+
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <TagBadge {...tag} key={tag._id} />
+        ))}
+      </div>
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          textStyles="body-medium text-dark400_light700"
+          img="/assets/icons/avatar.svg"
+          alt="user"
+          text={` asked ${humanizeDate(created_at)}`}
+          value={author.name}
+          href={`/profile/${author._id}`}
+          isAuthor
+        />
+        <Metric
+          textStyles="small-medium text-dark400_light800"
+          img="/assets/icons/like.svg"
+          alt="Upvotes"
+          text="Upvotes"
+          value={humanizeNumber(upvotes)}
+        />
+        <Metric
+          textStyles="small-medium text-dark400_light800"
+          img="/assets/icons/message.svg"
+          alt="message"
+          text="Answers"
+          value={humanizeNumber(answers.length)}
+        />
+        <Metric
+          textStyles="small-medium text-dark400_light800"
+          img="/assets/icons/eye.svg"
+          alt="eye"
+          text="Views"
+          value={humanizeNumber(views)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
