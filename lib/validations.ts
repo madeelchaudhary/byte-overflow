88b55c7d2d7aa3f40968c050a73465b581e38ce9
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-04 14:47:03 +0500

    Update validation rules for QuestionSchema

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,6 +2,6 @@ import * as z from "zod";
 
 export const QuestionSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(20).max(2000),
-  tags: z.array(z.string()).min(1).max(5),
+  description: z.string().min(100).max(2000),
+  tags: z.array(z.string().min(1).max(20)).min(1).max(5),
 });
