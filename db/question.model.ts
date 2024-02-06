e5be386a267f53ae8ceda04a7ae98f133ae30523
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-06 13:35:16 +0500

    Add question model to the database

---
+++ db/question.model.ts
@@ -0,0 +1,68 @@
+import { Schema, model, Document, models } from "mongoose";
+
+export interface IQuestion extends Document {
+  title: string;
+  description: string;
+  tags: Schema.Types.ObjectId[];
+  answers: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const questionSchema = new Schema<IQuestion>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tag",
+      },
+    ],
+    answers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Answer",
+      },
+    ],
+    views: {
+      type: Number,
+      default: 0,
+    },
+    upvotes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    downvotes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Question =
+  models.Question || model<IQuestion>("Question", questionSchema);
+
+new Question({});
