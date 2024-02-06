28f67de748eba88416acc3a414584fcc38e99522
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-06 13:35:20 +0500

    Add Tag model to the database

---
+++ db/tag.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model, Document, models } from "mongoose";
+
+export interface ITag extends Document {
+  name: string;
+  description: string;
+  questions: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const tagSchema = new Schema<ITag>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    questions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tag = models.Tag || model<ITag>("Tag", tagSchema);
