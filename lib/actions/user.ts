2e1db23c4438bd0d5b556a80004e196eeb7fa095
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-08 18:06:48 +0500

    Add user actions

---
+++ lib/actions/user.ts
@@ -0,0 +1,82 @@
+"use server";
+
+import Question from "@/db/question.model";
+import User from "@/db/user.model";
+import APIError from "../api-error";
+
+interface CreateUserParams {
+  clerkId: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName?: string;
+  avatar: string;
+}
+
+interface UpdateUserParams extends CreateUserParams {}
+
+interface DeleteUserParams {
+  clerkId: string;
+}
+
+export const createUser = async ({
+  clerkId,
+  username,
+  email,
+  avatar,
+  firstName,
+  lastName,
+}: CreateUserParams) => {
+  const user = new User({
+    clerkId: clerkId,
+    username: username,
+    email: email,
+    profile: {
+      name: `${firstName}${lastName ? ` ${lastName}` : ""}`,
+      avatar: avatar,
+    },
+  });
+
+  await user.save();
+};
+
+export const updateUser = async ({
+  clerkId,
+  username,
+  email,
+  avatar,
+  firstName,
+  lastName,
+}: UpdateUserParams) => {
+  const user = await User.findOne({ clerkId: clerkId });
+
+  if (!user) {
+    throw new APIError("User not found", 404);
+  }
+
+  user.username = username;
+  user.email = email;
+  user.profile.name = `${firstName}${lastName ? ` ${lastName}` : ""}`;
+  user.profile.avatar = avatar;
+
+  await user.save();
+
+  return user;
+};
+
+export const deleteUser = async ({ clerkId }: DeleteUserParams) => {
+  const user = await User.findOne({
+    clerkId: clerkId,
+  });
+
+  if (!user) {
+    throw new APIError("User not found", 404);
+  }
+
+  // Delete the user data
+  await Question.deleteMany({ author: user._id });
+
+  await User.deleteOne({ clerkId: clerkId });
+
+  return user;
+};
