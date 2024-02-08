4ceb2fd5fe09dc2f578780f55dd388fde44acd3d
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-08 18:06:21 +0500

    Refactor author and created_at properties in QuestionCard component

--- components/shared/questions/QuestionCard.tsx
+++ components/shared/questions/QuestionCard.tsx
@@ -11,8 +11,8 @@ interface Props {
   answers: Array<object>;
   views: number;
   upvotes: number;
-  author: { _id: string; name: string; picture: string };
-  created_at: Date;
+  author: { _id: string; profile: { name: string; picture: string } };
+  createdAt: Date | string;
 }
 
 const QuestionCard = ({
@@ -23,14 +23,14 @@ const QuestionCard = ({
   author,
   upvotes,
   views,
-  created_at,
+  createdAt,
 }: Props) => {
   return (
     <div className="card-wrapper rounded-xl p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 inline-block sm:hidden">
-            {humanizeDate(created_at)}
+            {humanizeDate(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
@@ -52,8 +52,8 @@ const QuestionCard = ({
           textStyles="body-medium text-dark400_light700"
           img="/assets/icons/avatar.svg"
           alt="user"
-          text={` asked ${humanizeDate(created_at)}`}
-          value={author.name}
+          text={` asked ${humanizeDate(createdAt)}`}
+          value={author.profile.name}
           href={`/profile/${author._id}`}
           isAuthor
         />
