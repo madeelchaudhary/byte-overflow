fb083f03a732d4a98fb2d722544fc9c63f620040
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-11 12:31:24 +0500

    Add tags page component

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,42 @@
+import NoResultsFound from "@/components/shared/NoResultsFound";
+import Filters from "@/components/shared/filter/Filters";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import TagCard from "@/components/shared/tags/TagCard";
+import { TagFilters } from "@/constants/filters";
+import { getTagsWithQuestionsCount } from "@/lib/data/tag";
+
+const page = async () => {
+  const tags = await getTagsWithQuestionsCount();
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Tags</h1>
+
+      <div className="mt-11 flex flex-col justify-between gap-5 sm:flex-row sm:items-center ">
+        <LocalSearch
+          className="flex-1"
+          placeholder="Search for tags..."
+          route="/tags"
+          img="/assets/icons/search.svg"
+          imgSide="left"
+        />
+        <Filters filters={TagFilters} className="min-h-14 sm:min-w-[170px]" />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4 ">
+        {tags.length > 0 ? (
+          tags.map((tag) => <TagCard key={tag._id} {...tag} />)
+        ) : (
+          <NoResultsFound
+            title="No tags found"
+            description="We couldn't find any tags matching your search"
+            link="/ask"
+            linkText="Ask a question to create a tag"
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default page;
