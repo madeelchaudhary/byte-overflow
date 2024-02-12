0abf938937131ea7d5cf3fdcc853d92824931ef0
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-12 18:41:16 +0500

    Add AnswerSchema to validations.ts

--- lib/validations.ts
+++ lib/validations.ts
@@ -5,3 +5,7 @@ export const QuestionSchema = z.object({
   description: z.string().min(100).max(2000),
   tags: z.array(z.string().min(1).max(20)).min(1).max(5),
 });
+
+export const AnswerSchema = z.object({
+  description: z.string().min(100).max(2000),
+});
