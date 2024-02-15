029739f22120e8e655aef2c891de7f85e7f7c0d4
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-15 15:05:20 +0500

    Add voting functionality and user information to question page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,14 +1,16 @@
 import dynamic from "next/dynamic";
 
 import Metric from "@/components/shared/Metric";
+import TagBadge from "@/components/shared/TagBadge";
+import Votes from "@/components/shared/Votes";
+import { getUserById } from "@/lib/actions/user";
 import { getQuestionById } from "@/lib/data/questions";
 import { humanizeDate, humanizeNumber } from "@/lib/utils";
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import TagBadge from "@/components/shared/TagBadge";
-import AnswerForm from "../_components/AnswerForm";
 import AllAnswers from "../_components/AllAnswers";
+import AnswerForm from "../_components/AnswerForm";
 
 const HtmlParser = dynamic(() => import("@/components/shared/HtmlParser"), {
   ssr: false,
@@ -21,9 +23,25 @@ interface Props {
 }
 
 const page = async ({ params: { id } }: Props) => {
-  const question = await getQuestionById(id);
-  const { title, description, tags, author, totalAnswers, views, createdAt } =
-    question;
+  const { userId } = auth();
+
+  const [question, user] = await Promise.all([
+    getQuestionById(id),
+    userId ? getUserById(userId) : null,
+  ]);
+
+  const {
+    _id,
+    title,
+    description,
+    tags,
+    author,
+    upvotes,
+    downvotes,
+    totalAnswers,
+    views,
+    createdAt,
+  } = question;
 
   return (
     <>
@@ -40,11 +58,20 @@ const page = async ({ params: { id } }: Props) => {
               height={22}
               className="rounded-full"
             />
-            <p className="paragraph-semibold text-dark300_light700 ">
+            <p className="paragraph-semibold text-dark300_light700">
               {author.profile.name}
             </p>
           </Link>
-          <div className="flex justify-end">Voting</div>
+          <div className="flex justify-end">
+            <Votes
+              type="question"
+              itemId={_id}
+              userId={user?._id?.toString()}
+              upvotes={upvotes}
+              downvotes={downvotes}
+              hasSaved={user ? user.saved.includes(_id) : undefined}
+            />
+          </div>
         </div>
         <h1 className="h2-bold text-dark200_light900 mt-3.5 w-full text-left">
           {title}
@@ -83,9 +110,13 @@ const page = async ({ params: { id } }: Props) => {
         ))}
       </div>
 
-      <AnswerForm />
+      <AllAnswers
+        questionId={id}
+        totalAnswers={totalAnswers}
+        userId={user?._id?.toString()}
+      />
 
-      <AllAnswers questionId={id} totalAnswers={totalAnswers} />
+      <AnswerForm />
     </>
   );
 };
