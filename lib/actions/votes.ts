9cb0a74e074f52641cbc2b71efc71d1cb5a4abba
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-15 15:03:27 +0500

    Add upvote and downvote functionality for answers

--- lib/actions/votes.ts
+++ lib/actions/votes.ts
@@ -4,6 +4,7 @@ import Question from "@/db/question.model";
 import dbConnect from "../dbConnect";
 import APIError from "../api-error";
 import { revalidatePath } from "next/cache";
+import Answer from "@/db/answer.model";
 
 export const upvoteQuestion = async (questionId: string, userId: string) => {
   try {
@@ -79,6 +80,88 @@ export const downvoteQuestion = async (questionId: string, userId: string) => {
     await question.save();
 
     revalidatePath(`/question/${questionId}`);
+  } catch (err) {
+    if (err instanceof APIError) {
+      return { error: err.message, status: err.code };
+    }
+    return { error: "Internal Server Error", status: 500 };
+  }
+};
+
+export const upvoteAnswer = async (answerId: string, userId: string) => {
+  try {
+    await dbConnect();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new APIError("User not found", 404);
+    }
+
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) {
+      throw new APIError("Answer not found", 404);
+    }
+
+    if (answer.upvotes.includes(user._id)) {
+      answer.upvotes = answer.upvotes.filter(
+        (id) => id.toString() !== user._id.toString()
+      );
+    } else {
+      answer.upvotes.push(user._id);
+    }
+
+    if (answer.downvotes.includes(user._id)) {
+      answer.downvotes = answer.downvotes.filter(
+        (id) => id.toString() !== user._id.toString()
+      );
+    }
+
+    await answer.save();
+
+    revalidatePath(`/question/${answer.question.toString()}`);
+  } catch (err) {
+    if (err instanceof APIError) {
+      return { error: err.message, status: err.code };
+    }
+    return { error: "Internal Server Error", status: 500 };
+  }
+};
+
+export const downvoteAnswer = async (answerId: string, userId: string) => {
+  try {
+    await dbConnect();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new APIError("User not found", 404);
+    }
+
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) {
+      throw new APIError("Answer not found", 404);
+    }
+
+    if (answer.downvotes.includes(user._id)) {
+      answer.downvotes = answer.downvotes.filter(
+        (id) => id.toString() !== user._id.toString()
+      );
+    } else {
+      answer.downvotes.push(user._id);
+    }
+
+    if (answer.upvotes.includes(user._id)) {
+      answer.upvotes = answer.upvotes.filter(
+        (id) => id.toString() !== user._id.toString()
+      );
+    }
+
+    await answer.save();
+
+    revalidatePath(`/question/${answer.question.toString()}`);
   } catch (err) {
     if (err instanceof APIError) {
       return { error: err.message, status: err.code };
