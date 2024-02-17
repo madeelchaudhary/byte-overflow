2c400403e76a00f13236990b80c681353db6d869
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-17 11:39:19 +0500

    Add getSavedQuestions function to user.ts

--- lib/data/user.ts
+++ lib/data/user.ts
@@ -1,7 +1,18 @@
 import User from "@/db/user.model";
+import Tag from "@/db/tag.model";
+import Question, { IQuestion } from "@/db/question.model";
 import dbConnect from "../dbConnect";
+import { FilterQuery } from "mongoose";
 
 interface GetUsersParams {
+  page?: number;
+  pageSize?: number;
+  search?: string;
+  filter?: string;
+}
+
+interface GetSavedQuestionsParams {
+  clerkId: string;
   page?: number;
   pageSize?: number;
   search?: string;
@@ -21,4 +32,38 @@ export const getUsers = async ({ page = 1, pageSize = 10 }: GetUsersParams) => {
   });
 
   return users;
+};
+
+export const getSavedQuestions = async ({
+  clerkId,
+  page = 1,
+  pageSize = 10,
+  search,
+}: GetSavedQuestionsParams) => {
+  await dbConnect();
+
+  const searchQuery: FilterQuery<IQuestion> = search
+    ? {
+        title: { $regex: new RegExp(search, "i") },
+      }
+    : {};
+
+  const user = await User.findOne({ clerkId }).populate({
+    path: "saved",
+    model: Question,
+    options: {
+      limit: pageSize,
+      skip: (page - 1) * pageSize,
+      sort: { createdAt: -1 },
+    },
+    populate: [
+      { path: "author", model: User },
+      { path: "tags", model: Tag },
+    ],
+    match: searchQuery,
+  });
+
+  if (!user) return null;
+
+  return JSON.parse(JSON.stringify(user.saved));
 };
