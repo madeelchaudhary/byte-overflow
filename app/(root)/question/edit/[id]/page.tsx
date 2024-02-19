092161886304cba2e51e217ea21d6dae230fcfeb
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-19 15:31:13 +0500

    Add page for editing questions

---
+++ app/(root)/question/edit/[id]/page.tsx
@@ -0,0 +1,35 @@
+import QuestionForm from "@/app/(root)/ask/_components/QuestionForm";
+import { getQuestionById } from "@/lib/data/questions";
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+import React from "react";
+
+const page = async ({ params: { id } }: { params: { id: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/sign-in");
+
+  const question = await getQuestionById(id);
+
+  if (!question) return notFound();
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
+
+      <div className="mt-9">
+        <QuestionForm
+          type="edit"
+          question={{
+            _id: question._id,
+            title: question.title,
+            description: question.description,
+            tags: question.tags.map((tag: any) => tag.name),
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default page;
