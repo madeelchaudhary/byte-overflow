fd678a58fef1b0c03b950efdc3fdcedec53eac1c
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-19 14:47:40 +0500

    Refactor answer actions and add answer delete action

--- lib/actions/answers.ts
+++ lib/actions/answers.ts
@@ -1,12 +1,13 @@
 "use server";
 
-import dbConnect from "@/lib/dbConnect";
-import Answer from "@/db/answer.model";
-import User from "@/db/user.model";
-import Question from "@/db/question.model";
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
 import * as z from "zod";
+
+import Answer from "@/db/answer.model";
+import Question from "@/db/question.model";
+import User, { IUser } from "@/db/user.model";
+import dbConnect from "@/lib/dbConnect";
 import APIError from "../api-error";
 import { AnswerSchema } from "../validations";
 
@@ -61,3 +62,43 @@ export async function createAnswer(params: CreateAnswerParams) {
 
   revalidatePath(`/question/${questionIdParam}`);
 }
+
+export const deleteAnswer = async (answerId: string, path?: string) => {
+  try {
+    await dbConnect();
+
+    const answer = await Answer.findById(answerId).populate("author");
+
+    if (!answer) {
+      throw new APIError("Answer not found", 404);
+    }
+
+    const { userId } = auth();
+    const author = answer.author as unknown as IUser;
+
+    if (!userId) {
+      throw new APIError("You must be logged in to delete an answer", 401);
+    }
+
+    if (userId !== author.clerkId) {
+      throw new APIError("You are not authorized to delete this answer", 403);
+    }
+
+    await Promise.all([
+      User.findByIdAndUpdate(author._id, {
+        $pull: { answers: answerId },
+      }),
+      Question.findByIdAndUpdate(answer.question, {
+        $pull: { answers: answerId },
+      }),
+      Answer.findByIdAndDelete(answerId),
+    ]);
+
+    revalidatePath(path || "/");
+  } catch (error) {
+    if (error instanceof APIError) {
+      return { error: error.message, status: error.code };
+    }
+    return { error: "Internal Server Error", status: 500 };
+  }
+};
