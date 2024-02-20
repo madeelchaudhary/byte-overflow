f1c8506e5a96c0b9d6fde42ae88e71669a7d9c73
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-20 13:16:32 +0500

    Refactor RightSidebar component to fetch hot questions and popular tags dynamically

--- components/shared/sidebars/RightSidebar.tsx
+++ components/shared/sidebars/RightSidebar.tsx
@@ -1,26 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import TagBadge from "../TagBadge";
+import { getHotQuestions } from "@/lib/data/questions";
+import { getHotTags } from "@/lib/data/tag";
 
-const RightSideBar = () => {
-  const hotQuestions = [
-    { _id: "1", title: "How to use Next.js with Tailwind CSS" },
-    { _id: "2", title: "Case insensitive 'Contains[X]' in C#" },
-    {
-      _id: "3",
-      title: "Django best practices for data migrations and testing",
-    },
-    { _id: "4", title: "Migration from AngularJS to Angular 12" },
-    { _id: "5", title: "How to use Prisma Client in a REST API" },
-  ];
+const RightSideBar = async () => {
+  const hotQuestions = await getHotQuestions({ page: 1, pageSize: 5 });
 
-  const popularTags = [
-    { _id: "1", name: "javascript", totalQuestions: 123 },
-    { _id: "2", name: "react", totalQuestions: 123 },
-    { _id: "3", name: "typescript", totalQuestions: 123 },
-    { _id: "4", name: "nextjs", totalQuestions: 123 },
-    { _id: "5", name: "prisma", totalQuestions: 123 },
-  ];
+  const popularTags = await getHotTags({ page: 1, pageSize: 5 });
 
   return (
     <aside className="light-border background-light900_dark200 custom-scrollbar sticky right-0 top-0 hidden h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none lg:w-[355px] xl:flex">
@@ -29,7 +16,7 @@ const RightSideBar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
