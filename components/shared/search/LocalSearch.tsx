4bb087bb9504a403e95ba6c4b59f1fd0d5b85085
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-21 14:31:00 +0500

    Update LocalSearch component to include search functionality

--- components/shared/search/LocalSearch.tsx
+++ components/shared/search/LocalSearch.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback, useEffect, useState } from "react";
 
 interface Props {
   img: string;
@@ -17,6 +18,39 @@ const LocalSearch = ({
   img,
   placeholder,
 }: Props) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const q = searchParams.get("q");
+  const [search, setSearch] = useState(q || "");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSearch = useCallback(() => {
+    const params = new URLSearchParams(searchParams);
+    if (search.trim()) {
+      const query = search.trim();
+      params.set("q", query);
+      const url = `${pathname}?${params.toString()}`;
+      router.push(url);
+    } else {
+      params.delete("q");
+      const url = `${pathname}?${params.toString()}`;
+      router.push(url);
+    }
+  }, [search, pathname, router, searchParams]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      onSearch();
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, onSearch]);
+
   return (
     <div
       className={`background-light800_darkgradient relative flex min-h-14 grow items-center gap-4 overflow-hidden rounded-xl px-4 ${className}`}
@@ -34,7 +68,8 @@ const LocalSearch = ({
         placeholder={placeholder}
         className="paragraph-regular no-focus placeholder absolute h-full w-full border-none bg-transparent pl-10 shadow-none outline-none"
         type="text"
-        onChange={() => {}}
+        value={search}
+        onChange={handleSearch}
       />
       {imgSide === "right" && (
         <Image
