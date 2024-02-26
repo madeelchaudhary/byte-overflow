ed0bc331974f62cc128917627225111f8cbee463
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-26 12:44:24 +0500

    Update validation string length limits

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,12 +2,12 @@ import * as z from "zod";
 
 export const QuestionSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(100).max(2000),
+  description: z.string().min(100).max(2555),
   tags: z.array(z.string().min(1).max(20)).min(1).max(5),
 });
 
 export const AnswerSchema = z.object({
-  description: z.string().min(100).max(2000),
+  description: z.string().min(100).max(2555),
 });
 
 export const ProfileSchema = z.object({
