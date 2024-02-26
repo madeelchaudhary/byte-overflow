57aeed96f19cae3f6b9b9202156c7feabb101dc4
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-26 11:55:27 +0500

    Refactor upvote and downvote questions to add reputation

--- lib/actions/votes.ts
+++ lib/actions/votes.ts
@@ -1,10 +1,16 @@
 "use server";
-import User from "@/db/user.model";
-import Question from "@/db/question.model";
-import dbConnect from "../dbConnect";
-import APIError from "../api-error";
 import { revalidatePath } from "next/cache";
+
 import Answer from "@/db/answer.model";
+import Question from "@/db/question.model";
+import User from "@/db/user.model";
+import Interaction from "@/db/interaction.model";
+import APIError from "../api-error";
+import dbConnect from "../dbConnect";
+import {
+  DOWNVOTE_QUESTION_REPUTATION,
+  UPVOTE_QUESTION_REPUTATION,
+} from "@/constants";
 
 export const upvoteQuestion = async (questionId: string, userId: string) => {
   try {
@@ -22,20 +28,54 @@ export const upvoteQuestion = async (questionId: string, userId: string) => {
       throw new APIError("Question not found", 404);
     }
 
+    let action: "upvote" | "none" = "none";
+
     if (question.upvotes.includes(user._id)) {
       question.upvotes = question.upvotes.filter(
         (id) => id.toString() !== user._id.toString()
       );
+
+      user.upvotes = user.upvotes.filter(
+        (id) => id.toString() !== question._id.toString()
+      );
     } else {
       question.upvotes.push(user._id);
+      user.upvotes.push(question._id);
+
+      action = "upvote";
     }
 
     if (question.downvotes.includes(user._id)) {
       question.downvotes = question.downvotes.filter(
         (id) => id.toString() !== user._id.toString()
       );
+
+      user.downvotes = user.downvotes.filter(
+        (id) => id.toString() !== question._id.toString()
+      );
     }
 
+    if (action === "upvote") {
+      const existingInteraction = await Interaction.findOne({
+        user: user._id,
+        question: question._id,
+        action: "upvote_question",
+      });
+
+      if (!existingInteraction) {
+        await Interaction.create({
+          user: user._id,
+          question: question._id,
+          action: "upvote_question",
+        });
+
+        await User.findByIdAndUpdate(question.author, {
+          $inc: { "profile.reputation": UPVOTE_QUESTION_REPUTATION },
+        });
+      }
+    }
+
+    await user.save();
     await question.save();
 
     revalidatePath(`/question/${questionId}`);
@@ -63,20 +103,54 @@ export const downvoteQuestion = async (questionId: string, userId: string) => {
       throw new APIError("Question not found", 404);
     }
 
+    let action: "downvote" | "none" = "none";
+
     if (question.downvotes.includes(user._id)) {
       question.downvotes = question.downvotes.filter(
         (id) => id.toString() !== user._id.toString()
       );
+
+      user.downvotes = user.downvotes.filter(
+        (id) => id.toString() !== question._id.toString()
+      );
     } else {
       question.downvotes.push(user._id);
+      user.downvotes.push(question._id);
+
+      action = "downvote";
     }
 
     if (question.upvotes.includes(user._id)) {
       question.upvotes = question.upvotes.filter(
         (id) => id.toString() !== user._id.toString()
       );
+
+      user.upvotes = user.upvotes.filter(
+        (id) => id.toString() !== question._id.toString()
+      );
     }
 
+    if (action === "downvote") {
+      const existingInteraction = await Interaction.findOne({
+        user: user._id,
+        question: question._id,
+        action: "downvote_question",
+      });
+
+      if (!existingInteraction) {
+        await Interaction.create({
+          user: user._id,
+          question: question._id,
+          action: "downvote_question",
+        });
+
+        await User.findByIdAndUpdate(question.author, {
+          $inc: { "profile.reputation": -DOWNVOTE_QUESTION_REPUTATION },
+        });
+      }
+    }
+
+    await user.save();
     await question.save();
 
     revalidatePath(`/question/${questionId}`);
