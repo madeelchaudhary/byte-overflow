51b300a08c1cd4fbe94227d467787b24f2c97d1a
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-28 15:24:37 +0500

    Add AI answer generation functionality

--- lib/actions/commons.ts
+++ lib/actions/commons.ts
@@ -43,3 +43,133 @@ export const globalSearch = async (query: string, filter?: string) => {
     throw new Error("An error occurred while fetching the results.");
   }
 };
+
+export const generateAIAnswer = async (questionId: string) => {
+  try {
+    const answerWith = process.env.GENERATE_ANSWER_WITH;
+
+    if (answerWith === "gemini") {
+      return generateGeminiAnswer(questionId);
+    } else if (answerWith === "openai") {
+      return generateOpenAIAnswer(questionId);
+    } else {
+      throw new Error("Answer generation service not found.");
+    }
+  } catch (error) {
+    throw new Error("An error occurred while generating the answer.");
+  }
+};
+
+async function generateGeminiAnswer(questionId: string) {
+  try {
+    await dbConnect();
+
+    const question = await Question.findById(questionId).lean();
+
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+
+    const geminiUrl = process.env.GEMINI_API_URL;
+    const res = await fetch(geminiUrl + `?key=${process.env.GEMINI_API_KEY}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: `
+                Below is the question that needs to be answered:
+
+                Title: ${question.title}
+                Explanation in WYSIWYG format: ${question.description}
+
+                Please provide an answer to the question.
+
+                For example, you can provide a code snippet, a link to a resource, or a brief explanation.
+              `,
+              },
+            ],
+            role: "user",
+          },
+        ],
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.status !== 200) {
+      console.log(data);
+      throw new Error("Gemini API failed to generate an answer.");
+    }
+
+    const content = data.candidates[0].content.parts[0].text;
+
+    return { content };
+  } catch (error) {
+    console.log(error);
+    throw new Error("An error occurred while generating the answer.");
+  }
+}
+
+async function generateOpenAIAnswer(questionId: string) {
+  try {
+    await dbConnect();
+
+    const question = await Question.findById(questionId).lean();
+
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+
+    const openaiUrl = process.env.OPENAI_API_URL!;
+    const res = await fetch(openaiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a developer trying to answer a question. Be helpful and concise.",
+          },
+          {
+            role: "user",
+            content: `
+              Below is the question that needs to be answered:
+
+              ''' 
+                Title: ${question.title}
+                Explanation in WYSIWYG format: ${question.description}
+              '''
+
+              Please provide an answer to the question.
+
+              For example, you can provide a code snippet, a link to a resource, or a brief explanation.
+            `,
+          },
+        ],
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.status !== 200) {
+      throw new Error(data.error.message);
+    }
+
+    const content = data.choices[0].message.content;
+
+    return { content };
+  } catch (error) {
+    console.log(error);
+    throw new Error("An error occurred while generating the answer.");
+  }
+}
