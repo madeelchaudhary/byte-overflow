363f206f6946ebea4cbef1f42fe1463bdb734ac9
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-29 13:51:43 +0500

    Add loading component for community page

---
+++ app/(root)/community/loading.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const loading = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+
+      <div className="mt-11 flex flex-col justify-between gap-5 sm:flex-row sm:items-center">
+        <Skeleton className="h-14 flex-1 max-sm:min-w-full max-sm:flex-auto " />
+        <Skeleton className="h-14 max-sm:min-w-full sm:min-w-[170px]" />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+          <Skeleton
+            key={i}
+            className="h-[308px] w-full rounded-2xl max-xs:min-w-full xs:w-[260px]"
+          />
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default loading;
